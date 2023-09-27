34467ec70abc0d3cb61d89d8d14a4b8d4c7761a3
BayronSoto-19 <b.soto54@alumnos.santotomas.cl>
2023-09-27 15:26:33 -0300

    arreglos a algunas activity

---
+++ app/src/main/java/com/example/spacesport1/TaekwondoPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.spacesport1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaekwondoPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taekwondo_principal);
+    }
+}
